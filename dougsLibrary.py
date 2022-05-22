6144a76fd25cc38d53761569a23b52ca1d47bdf3
CyberDoug <105928061+CyberDoug@users.noreply.github.com>
2022-05-22 10:23:19 -0500

    Add files via upload

---
+++ dougsLibrary.py
@@ -0,0 +1,10 @@
+import sys
+
+def importedFunction():
+    print("This is being printed by a function that lives inside of dougsLibrary!")
+
+def main():
+    print("This is being the main() in dougsLibrary!")
+
+if __name__ == "__main__":
+    main()
